d79ece09e5f8c415b92f356533060b7b7684e347
Purity Wambua <puritywamb@gmail.com>
2024-10-02 09:57:23 +0300

    sqlalchemy

--- server/app.py
+++ server/app.py
@@ -21,6 +21,44 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def get_earthquake(id):
+    earthquake = Earthquake.query.get(id)
+    if earthquake is None:
+        #return 404 error
+        return make_response('{"message": "Earthquake ' + str(id) + ' not found."}', 404, {'Content-Type': 'application/json'})
+    #Return the earthquake details in JSON format if found
+    response_data = {
+        "id": earthquake.id,
+        "location": earthquake.location,
+        "magnitude": earthquake.magnitude,
+        "year" : earthquake.year
+    }
+    #manually create a JSON string
+    return make_response(str(response_data).replace("'", '"'), 200, {'Content-Type': 'application/json'})
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def get_earthquakes_by_magnitude(magnitude):
+    quakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+
+    quakes_data = [{
+        "id": quake.id,
+        "location": quake.location,
+        "magnitude": quake.magnitude,
+        "year": quake.year
+                           
+        } for quake in quakes]
+    
+    response_data = {
+        "count": len(quakes_data),
+        "quakes": quakes_data
+    }
+
+
+     # Manually create a JSON string
+    json_response = str(response_data).replace("'", '"')
+    return make_response(json_response, 200, {'Content-Type': 'application/json'})
+
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -7,3 +7,13 @@ metadata = MetaData()
 db = SQLAlchemy(metadata=metadata)
 
 # Add models here
+class Earthquake(db.Model, SerializerMixin):
+    __tablename__= "earthquakes"#define table name
+
+    id= db.Column(db.Integer, primary_key=True)
+    magnitude= db.Column(db.Float)#magnitude column
+    location = db.Column(db.String)#location column
+    year = db.Column(db.Integer)#year column
+
+    def __repr__(self):
+        return f"<Earthquake {self.id}, {self.magnitude}, {self.location}, {self.year}>" #return a formatted strin
